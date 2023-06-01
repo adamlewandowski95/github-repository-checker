be07e80cf61b7ca7460066e415cc2915c03c411e
Adam Lewandowski <Alewandowski1995@gmail.com>
2023-06-01 17:26:54 +0200

    Initialize project

---
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/GithubRepositoryCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.adamlewandowski.githubrepositorychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositoryCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamlewandowski/githubrepositorychecker/GithubRepositoryCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamlewandowski.githubrepositorychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
