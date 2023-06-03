7d1dd0b41b94a99dd6d7ed2d9774c6d38900adb0
Adam Lewandowski <Alewandowski1995@gmail.com>
2023-06-04 00:02:22 +0200

    Shorten name of application

--- src/main/java/com/adamlewandowski/githubrepositorychecker/GithubRepositoryChecker.java
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/GithubRepositoryChecker.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GithubRepositoryCheckerApplication {
+public class GithubRepositoryChecker {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GithubRepositoryCheckerApplication.class, args);
+		SpringApplication.run(GithubRepositoryChecker.class, args);
 	}
 
 }
