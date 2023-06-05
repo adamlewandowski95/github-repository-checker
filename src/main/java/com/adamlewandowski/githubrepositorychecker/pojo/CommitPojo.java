8e33f312e558cd6c66b6f839580457209c8a55f8
Adam Lewandowski <Alewandowski1995@gmail.com>
2023-06-05 16:16:51 +0200

    Add pojo classes

---
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/pojo/CommitPojo.java
@@ -0,0 +1,55 @@
+package com.adamlewandowski.githubrepositorychecker.pojo;
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "sha",
+        "url"
+})
+
+public class CommitPojo {
+
+    @JsonProperty("sha")
+    private String sha;
+    @JsonProperty("url")
+    private String url;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<>();
+
+    @JsonProperty("sha")
+    public String getSha() {
+        return sha;
+    }
+
+    @JsonProperty("sha")
+    public void setSha(String sha) {
+        this.sha = sha;
+    }
+
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+    @JsonProperty("url")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
+
+
