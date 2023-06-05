41656af6c2207cc5932e98390c61e62dae3cca35
Adam Lewandowski <Alewandowski1995@gmail.com>
2023-06-05 16:19:04 +0200

    Add simple RestController

--- src/main/java/com/adamlewandowski/githubrepositorychecker/controller/RepositoriesRestController.java
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/controller/RepositoriesRestController.java
@@ -1,6 +1,6 @@
 package com.adamlewandowski.githubrepositorychecker.controller;
 
-import com.adamlewandowski.githubrepositorychecker.domain.Repositories;
+import com.adamlewandowski.githubrepositorychecker.controller.dto.RepositoryInformationDto;
 import com.adamlewandowski.githubrepositorychecker.service.RepositoriesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +12,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/repositories")
 @RequiredArgsConstructor
-public class RepositoriesController {
+public class RepositoriesRestController {
 
     private final RepositoriesService repositoriesService;
 
     @GetMapping
-    public List<Repositories> getAll(){
-        return repositoriesService.getRepositories();
+    public List<RepositoryInformationDto> getAll(){
+        return repositoriesService.getAllOwnerRepositories("adamlewandowski95");
     }
 
 }
