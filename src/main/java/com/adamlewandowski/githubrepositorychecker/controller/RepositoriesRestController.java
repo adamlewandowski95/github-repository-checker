53f6cf36ae2403a6d09f6a941a126ed2604648ad
Adam Lewandowski <Alewandowski1995@gmail.com>
2023-06-07 01:04:56 +0200

    Simplify RestController

--- src/main/java/com/adamlewandowski/githubrepositorychecker/controller/RepositoriesRestController.java
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/controller/RepositoriesRestController.java
@@ -3,7 +3,9 @@ package com.adamlewandowski.githubrepositorychecker.controller;
 import com.adamlewandowski.githubrepositorychecker.controller.dto.RepositoryInformationDto;
 import com.adamlewandowski.githubrepositorychecker.service.GithubRepositoriesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,8 @@ public class RepositoriesRestController {
 
     private final GithubRepositoriesService githubRepositoriesService;
 
-    @GetMapping
-    public List<RepositoryInformationDto> getAll(){
-        return githubRepositoriesService.getAllOwnerRepositories("adamlewandowski95");
+    @GetMapping(value = "/{username}", produces = "application/json")
+    public ResponseEntity<List<RepositoryInformationDto>> getRepositoriesList(@PathVariable("username") String username) {
+        return ResponseEntity.ok(githubRepositoriesService.getAllOwnerRepositories(username));
     }
-
 }
