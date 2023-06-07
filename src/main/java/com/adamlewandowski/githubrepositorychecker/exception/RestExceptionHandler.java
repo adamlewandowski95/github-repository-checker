7bcdea54fe01f53e7ce7ab2e56e111f96379a696
Adam Lewandowski <Alewandowski1995@gmail.com>
2023-06-07 17:59:03 +0200

    Change error handling class

--- src/main/java/com/adamlewandowski/githubrepositorychecker/exception/RestExceptionHandler.java
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/exception/RestExceptionHandler.java
@@ -1,30 +1,45 @@
 package com.adamlewandowski.githubrepositorychecker.exception;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
 
-@RestControllerAdvice
+@AllArgsConstructor
+@ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(UnsuportedMediaTypeException.class)
-    public ResponseEntity<Object> notFoundHandler(UnsuportedMediaTypeException exception) {
-        ApiError apiError = new ApiError(NOT_ACCEPTABLE, exception.getMessage());
-        return new ResponseEntity<>(apiError, NOT_ACCEPTABLE);
-    }
+    ObjectMapper objectMapper;
 
     @ExceptionHandler(NoSuchUserException.class)
-    public ResponseEntity<Object> notFoundHandler(NoSuchUserException exception) {
+    public ResponseEntity<Object> noSuchUser(NoSuchUserException exception) {
         ApiError apiError = new ApiError(NOT_FOUND, exception.getMessage());
         return new ResponseEntity<>(apiError, NOT_FOUND);
     }
 
     @ExceptionHandler(UnexpectedStatusCodeException.class)
-    public ResponseEntity<Object> notFoundHandler(UnexpectedStatusCodeException exception) {
+    public ResponseEntity<Object> unexpectedStatusCode(UnexpectedStatusCodeException exception) {
         ApiError apiError = new ApiError(I_AM_A_TEAPOT, exception.getMessage());
         return new ResponseEntity<>(apiError, I_AM_A_TEAPOT);
     }
+
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ResponseEntity<Object> handleHttpMediaTypeNotAcceptable() throws JsonProcessingException {
+        ApiError apiError = new ApiError(NOT_ACCEPTABLE, "Response can be generated only in json format");
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+        responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(objectMapper.writeValueAsString(apiError), responseHeaders, NOT_ACCEPTABLE);
+    }
+
 }
--- src/main/java/com/adamlewandowski/githubrepositorychecker/exception/UnsuportedMediaTypeException.java
+++
@@ -1,8 +0,0 @@
-package com.adamlewandowski.githubrepositorychecker.exception;
-
-public class UnsuportedMediaTypeException extends RuntimeException {
-
-    public UnsuportedMediaTypeException(String message) {
-        super(message);
-    }
-}
