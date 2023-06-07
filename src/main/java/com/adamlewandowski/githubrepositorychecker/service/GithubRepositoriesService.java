53478b290da9eb2bd34b2a1f8ec07efcec8bc9ab
Adam Lewandowski <Alewandowski1995@gmail.com>
2023-06-07 17:57:09 +0200

    Add new version of service
    Now it is possible to handle 404 exception and
    inform about other exceptions occurred while using API

--- src/main/java/com/adamlewandowski/githubrepositorychecker/service/GithubRepositoriesService.java
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/service/GithubRepositoriesService.java
@@ -2,11 +2,16 @@ package com.adamlewandowski.githubrepositorychecker.service;
 
 import com.adamlewandowski.githubrepositorychecker.controller.dto.BranchInformationDto;
 import com.adamlewandowski.githubrepositorychecker.controller.dto.RepositoryInformationDto;
+import com.adamlewandowski.githubrepositorychecker.exception.NoSuchUserException;
+import com.adamlewandowski.githubrepositorychecker.exception.UnexpectedStatusCodeException;
 import com.adamlewandowski.githubrepositorychecker.pojo.BranchPojo;
 import com.adamlewandowski.githubrepositorychecker.pojo.RepositoryPojo;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -14,19 +19,24 @@ import java.util.List;
 @NoArgsConstructor
 public class GithubRepositoriesService {
 
-    private final WebClient.Builder webClientBuilder = WebClient.builder().baseUrl("https://api.github.com");
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
-    public List<RepositoryInformationDto> getAllOwnerRepositories(String owner) {
+    public List<RepositoryInformationDto> getAllOwnerRepositories(String owner) throws NoSuchUserException, UnexpectedStatusCodeException {
         List<RepositoryPojo> repositoryPojo = getRepositoryInformationFromGithub(owner);
         return prepareRepositoryInformationDto(repositoryPojo);
     }
 
-    private List<RepositoryPojo> getRepositoryInformationFromGithub(String owner) {
+    private List<RepositoryPojo> getRepositoryInformationFromGithub(String owner) throws NoSuchUserException, UnexpectedStatusCodeException {
         return webClientBuilder.build()
                 .get()
-                .uri("/users/" + owner + "/repos?type=owner")
+                .uri(String.format("/users/%s/repos?type=owner", owner))
                 .retrieve()
                 .bodyToFlux(RepositoryPojo.class)
+                .onErrorResume(WebClientResponseException.class,
+                        ex -> ex.getStatusCode().value() == 404 ?
+                                Mono.error(new NoSuchUserException("There is no such user in Github")) :
+                                Mono.error(new UnexpectedStatusCodeException("Unexpected error from API")))
                 .collectList()
                 .block();
     }
@@ -45,7 +55,7 @@ public class GithubRepositoriesService {
     private List<BranchPojo> getBranchesForRepositoryFromGithub(String owner, String repositoryName) {
         return webClientBuilder.build()
                 .get()
-                .uri(String.format("/repos/%s/%s/branches",owner, repositoryName))
+                .uri(String.format("/repos/%s/%s/branches", owner, repositoryName))
                 .retrieve()
                 .bodyToFlux(BranchPojo.class)
                 .collectList()
