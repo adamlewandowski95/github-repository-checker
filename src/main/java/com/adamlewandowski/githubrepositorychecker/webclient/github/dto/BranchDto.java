cfd54cbd7f5d75e966977bbb50cfbcf7c89e383a
Adam Lewandowski <Alewandowski1995@gmail.com>
2023-06-08 23:47:40 +0200

    Add default constructor to Dto classes

--- src/main/java/com/adamlewandowski/githubrepositorychecker/webclient/github/dto/BranchDto.java
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/webclient/github/dto/BranchDto.java
@@ -3,9 +3,11 @@ package com.adamlewandowski.githubrepositorychecker.webclient.github.dto;
 import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class BranchDto {
 
     private String name;
--- src/main/java/com/adamlewandowski/githubrepositorychecker/webclient/github/dto/OwnerDto.java
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/webclient/github/dto/OwnerDto.java
@@ -2,9 +2,11 @@ package com.adamlewandowski.githubrepositorychecker.webclient.github.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OwnerDto {
 
     private String login;
