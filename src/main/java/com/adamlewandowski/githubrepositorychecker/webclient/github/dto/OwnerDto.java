bde01339ce49c07ecde01b770f00a4b6117a6b03
Adam Lewandowski <Alewandowski1995@gmail.com>
2023-06-08 23:15:58 +0200

    Add AllArgsConstructor to Dto received from API

--- src/main/java/com/adamlewandowski/githubrepositorychecker/webclient/github/dto/OwnerDto.java
+++ src/main/java/com/adamlewandowski/githubrepositorychecker/webclient/github/dto/OwnerDto.java
@@ -1,8 +1,10 @@
 package com.adamlewandowski.githubrepositorychecker.webclient.github.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class OwnerDto {
 
     private String login;
